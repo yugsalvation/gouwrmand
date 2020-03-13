33c5f7c303f44d9a41c311e936e63e482d7798ac
yugsalvation <33853459+yugsalvation@users.noreply.github.com>
2020-03-12 23:19:41 -0400

    build 1

---
+++ gouwrmand/src/test/java/com/spring/gouwrmand/GouwrmandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.gouwrmand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GouwrmandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
