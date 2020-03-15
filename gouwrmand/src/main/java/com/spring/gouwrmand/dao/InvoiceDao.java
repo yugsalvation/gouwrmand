c3f9c3f371172d7183534acf028356cb550701e3
yugsalvation <33853459+yugsalvation@users.noreply.github.com>
2020-03-15 15:48:01 -0400

    changes done

--- gouwrmand/src/main/java/com/spring/gouwrmand/dao/InvoiceDao.java
+++ gouwrmand/src/main/java/com/spring/gouwrmand/dao/InvoiceDao.java
@@ -6,7 +6,7 @@ import java.util.List;
 import com.spring.gouwrmand.entity.Invoice;
 
 public interface InvoiceDao {
-	public Invoice getInvoice(String invoiceid);
+	public Invoice getInvoice(int invoiceid);
 	public void addInvoice(Invoice in);
 	public List<Invoice> getInvoices(Date from, Date to);
 	public void updateInvoice(Invoice in);
